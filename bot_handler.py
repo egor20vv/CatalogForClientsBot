0f3b9fda0be3f3ce067ee544c9a88239c2e09f06
EgorVv <egor20vv@gmail.com>
2021-02-11 19:30:50 +0700

    First test

--- bot_handler.py
+++ bot_handler.py
@@ -7,7 +7,12 @@ bot = telebot.TeleBot(configs.TOKEN, parse_mode=None)
 
 @bot.message_handler(commands=['start'])
 def __start_handler(message: telebot.types.Message):
-    bot.send_message(message.chat.id, "Я родился", reply_to_message_id=message.id)
+    telebot.types.ReplyKeyboardRemove()
+    bot.send_message(message.chat.id, text="Я родился",
+                     reply_to_message_id=message.id,
+                     reply_markup=telebot.types.ReplyKeyboardMarkup(resize_keyboard=3)
+                     .add(telebot.types.KeyboardButton(text='/help')))
+    print("Пользователь '%s' получил сообщение '%s'" % (message.from_user.id, 'Я родился', ))
 
 
 def start_polling():
