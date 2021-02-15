f6002b82908dabb29e0a870ab5f5714737281577
EgorVv <egor20vv@gmail.com>
2021-02-15 22:36:27 +0700

    Visual part was changed

--- bot_configs.py
+++ bot_configs.py
@@ -3,10 +3,11 @@ import telebot
 REMOVED_MARKUP = telebot.types.ReplyKeyboardRemove()
 
 MAIN_PAGE_MARKUP = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=False) \
-    .row(telebot.types.KeyboardButton('🍭 Поиск'),
-         telebot.types.KeyboardButton('🍱 Категории')) \
+    .row(telebot.types.KeyboardButton('🍭 Поиск'))\
+    .row(telebot.types.KeyboardButton('🍱 Категории'),
+         telebot.types.KeyboardButton('🍥 Настройки')) \
     .row(telebot.types.KeyboardButton('🍻 Помощь'),
-         telebot.types.KeyboardButton('🍥 Настройки'))
+         telebot.types.KeyboardButton('🥂 Поделиться'))
 
 BACK_TO_MAIN_PAGE_MARKUP = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True) \
     .add(telebot.types.KeyboardButton('↩ Вернуться'))
